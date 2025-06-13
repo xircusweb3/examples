b4562bbb8e30343a058f9474beb45d3302708557
Mikey <development@xircus.app>
2025-06-13 19:02:30 +0800

    Updated base template

--- javascript/base-ts/pages/index.tsx
+++ javascript/base-ts/pages/index.tsx
@@ -1,11 +1,16 @@
 import { AppLayout } from "@/layouts/AppLayout";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Button, Heading } from "@chakra-ui/react";
+import { useWallet } from "@xw3/maki-ts";
 
 export default function Index() {
+  const wallet = useWallet()
+
   return (
     <AppLayout>
       <Box px={4}>
         <Heading>Base</Heading>        
+        <Box>{wallet.address}</Box>
+        <Button onClick={wallet.connect}>Connect</Button>
       </Box>
     </AppLayout>
   )
